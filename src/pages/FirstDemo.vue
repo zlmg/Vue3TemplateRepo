b4617389d6655ab68ab139e27c54c64c8cd460b7
zlmg <zlmg4@163.com>
2024-08-02 18:09:59 +0800

    Add: first demo

---
+++ src/pages/FirstDemo.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="first-demo">
+    first demo
+  </div>
+</template>
+
+<style scoped>
+  .first-demo {
+    background-color: red;
+  }
+</style>
