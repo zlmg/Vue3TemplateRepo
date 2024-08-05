0902509586aa6f5c821169cc17e6bfec9b4857e8
zlmg <zlmg4@163.com>
2024-08-05 16:05:13 +0800

    Add: 配置element-plus

--- src/pages/FirstDemo.vue
+++ src/pages/FirstDemo.vue
@@ -4,12 +4,19 @@
 
 <template>
   <div class="first-demo">
-    first demo
+    <span class="text">first demo</span>
+    <el-button type="primary">ElementPlus按钮</el-button>
   </div>
 </template>
 
 <style scoped>
   .first-demo {
+    height: 200px;
     background-color: red;
+    display: flex;
+    justify-content: flex-start;
+    .text {
+      color: white;
+    }
   }
 </style>
